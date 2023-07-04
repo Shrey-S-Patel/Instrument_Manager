4adfff968e6041235dbe53948130fbc4f1eba448
Shrey <ptl.shreys@gmail.com>
2023-07-04 12:54:53 +0300

    Student Report feature added and creation of student reports also works.

--- app/src/main/java/com/bdo/shrey/instrumentmanager/ViewStudentActivity.java
+++ app/src/main/java/com/bdo/shrey/instrumentmanager/ViewStudentActivity.java
@@ -1,10 +1,23 @@
 package com.bdo.shrey.instrumentmanager;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.TextPaint;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,6 +33,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.app.ActivityCompat;
 
 import com.bdo.shrey.instrumentmanager.Models.Category;
 import com.bdo.shrey.instrumentmanager.Models.History;
@@ -38,6 +52,10 @@ import com.google.firebase.database.ValueEventListener;
 import com.orhanobut.dialogplus.DialogPlus;
 import com.orhanobut.dialogplus.ViewHolder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -71,8 +89,8 @@ public class ViewStudentActivity extends AppCompatActivity {
     private DatabaseReference cat_ref = FirebaseDatabase.getInstance().getReference("Categories");
     private DatabaseReference stud_ref = FirebaseDatabase.getInstance().getReference("Students");
     private DatabaseReference basic_ref = FirebaseDatabase.getInstance().getReference();
-
-
+    Bitmap bmp, scaledBmp;
+    int page_width = 1200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -109,8 +127,16 @@ public class ViewStudentActivity extends AppCompatActivity {
         receive_btn = findViewById(R.id.receivebtn);
         edit_btn = findViewById(R.id.editbtn);
         delete_btn = findViewById(R.id.deletebtn);
-        generate_btn = findViewById(R.id.generatebtn);
 
+        generate_btn = findViewById(R.id.generatebtn);
+        generate_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ghetto);
+                scaledBmp = Bitmap.createScaledBitmap(bmp, 490, 226, false );
+                createPDF();
+            }
+        });
 
         Bundle extras = getIntent().getExtras();
         String id = extras.getString("id");
@@ -141,6 +167,7 @@ public class ViewStudentActivity extends AppCompatActivity {
                     if (snapshot.child(id).exists()) {
                         Student student = snapshot.child(id).getValue(Student.class);
                         name.setText(student.getName());
+                        s_id = student.getId();
                         code.setText(student.getId());
                         location.setText(student.getLocation());
                         status.setText(student.getStatus());
@@ -522,6 +549,121 @@ public class ViewStudentActivity extends AppCompatActivity {
 
                 }
             });
+        }
+    }
+
+    public boolean isStoragePermissionGranted() {
+        String TAG = "Storage Permission";
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (this.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG, "Permission is granted");
+                return true;
+            } else {
+                Log.v(TAG, "Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
+
+    private void createPDF() {
+        PdfDocument document = new PdfDocument();
+        Paint paint = new Paint();
+        TextPaint title = new TextPaint();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(1200, 2010, 1).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        //To draw to the page we need a Canvas
+        Canvas canvas = page.getCanvas();
+        int centreX;
+        centreX = (1200  - scaledBmp.getScaledWidth(canvas)) /2;
+        canvas.drawBitmap(scaledBmp, centreX, 50, paint);
+        title.setTextAlign(Paint.Align.CENTER);
+        title.setTypeface(Typeface.create("Arial", Typeface.BOLD));
+        title.setTextSize(60);
+        canvas.drawText("STUDENT REPORT", page_width/2, 360, title);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(3);
+        canvas.drawLine(320, 370, page_width-320, 370, paint);
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTypeface(Typeface.DEFAULT);
+        paint.setTextAlign(Paint.Align.LEFT);
+        paint.setTextSize(30f);
+        paint.setColor(Color.BLACK);
+        canvas.drawText("Student Name: " + name.getText(), 50, 490, paint);
+        canvas.drawText("Student ID: " + code.getText(), 50, 550, paint);
+        canvas.drawText("Student Location: " + location.getText(), 50, 610, paint);
+        canvas.drawText("Student Assigned: " + assigned.getText(), 50, 670, paint);
+
+        paint.setTextAlign(Paint.Align.RIGHT);
+        canvas.drawText("Student Status: " + status.getText(), page_width-50, 490, paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(3);
+        canvas.drawLine(70, 850, 400, 850, paint);
+
+        paint.setTextAlign(Paint.Align.LEFT);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        canvas.drawText(current.getText().toString(), 50, 730, paint);
+        canvas.drawText("Instruments Learned", 90, 840, paint);
+//        history.draw(canvas);
+        paint.setTextAlign(Paint.Align.LEFT);
+        paint.setTypeface(Typeface.DEFAULT);
+        paint.setTextSize(30f);
+        paint.setColor(Color.BLACK);
+
+        int table_height = 910;
+        for (int i = 0; i<history_list.size(); i++){
+            canvas.drawText(history_list.get(i), 120, table_height, paint);
+            table_height = table_height+55;
+        }
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(3);
+        canvas.drawLine(50, 1900, page_width-50, 1900, paint);
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD_ITALIC));
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setTextSize(30f);
+        paint.setColor(Color.BLUE);
+        canvas.drawText("Reporting done by A BDO System.", page_width/2, 1950, paint);
+
+        //Finish the page to close the writing phase
+        document.finishPage(page);
+
+        //Writing this File to custom folder for AOM PDFs
+        if (isStoragePermissionGranted()){
+            String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString();
+            File aom_dir = new File(root, "/AoM Docs");
+            if (!aom_dir.exists()){
+                aom_dir.mkdir();
+            }
+            String pdf_name = s_id + "_Report.pdf";
+            File pdf = new File(aom_dir, pdf_name);
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pdf);
+                document.writeTo(fos);
+                document.close();
+                fos.close();
+                Toast.makeText(this, "Created " + pdf_name+ " Successfully!", Toast.LENGTH_SHORT).show();
+            }
+            catch (FileNotFoundException e){
+                Log.d("WriteLog", "Error while writing: "+ e.toString());
+                throw new RuntimeException(e);
+            }
+            catch (IOException e){
+                throw new RuntimeException(e);
+            }
         }
     }
 }
